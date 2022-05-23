c4510cc65512629d1426bf7bf2686e30df7311eb
Taís Christofani <taiskc@gmail.com>
2022-05-22 21:31:21 -0300

    Implementando o Novo Projeto: Ajustando o Resto do Sistema

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'home.html')
@@ -8,5 +8,7 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'])
     return redirect('/lists/the-only-list-in-the-world/')
 
-def view_list(request):
-    pass
+def new_list(request):
+    my_list = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=my_list)
+    return redirect('/lists/the-only-list-in-the-world/')
