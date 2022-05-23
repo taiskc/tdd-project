c20fd83bd9ad633edf41a1e80ab14b20725a3573
Taís Christofani <taiskc@gmail.com>
2022-05-22 21:34:48 -0300

    Implementando o Novo Projeto: Cada Lista Deve Ter Uma URL

--- lists/views.py
+++ lists/views.py
@@ -5,10 +5,11 @@ def home_page(request):
     return render(request, 'home.html')
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-only-list-in-the-world/')
-
-def new_list(request):
     my_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{my_list.id}/')
+
+def view_list(request, list_id):
+    my_list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=my_list)
+    return render(request, 'list.html', {'items': items})
